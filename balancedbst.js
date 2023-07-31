799bb47cf4333306dacbcef3b6d1d476af3678ef
Mark Elliott <115184056+Mark-Elliott5@users.noreply.github.com>
2023-07-31 10:52:22 -0400

    Builds and prints test tree

--- balancedbst.js
+++ balancedbst.js
@@ -1,26 +1,37 @@
-const Node = (left, value, right) => {
-    return {left, value, right};
+const Node = (data, left = null, right = null) => {
+    return {data, left, right};
 }
 
 const Tree = (array) => {
+    debugger;
+    array.sort((a, b) => a - b);
     const root = buildTree(array);
-    return {root};
+    return root;
 }
 
 const buildTree = (array) => {
-    if (array.length > 1) {
-        if (array.length % 2 === 0) {
-            const left = array.slice(0, (array.length/2)-1);
-            const value = array[array.length/2];
-            const right = array.slice((array.length/2)+1, array.length);
-            return buildTree(left, value, right);
-        } else {
-            const left = array1.slice(0, Math.floor(array1.length/2));
-            const value = array[Math.floor(array1.length/2)]
-            const right = array1.slice(Math.floor(array1.length/2));
-            return buildTree(left, value, right);
-        }
-    } else {
-        return Node(null, array[0], null);
+    if (array.length === 0) {
+        return null;
     }
+    const left = array.slice(0, Math.floor(array.length/2));
+    const value = array[Math.floor(array.length/2)]
+    const right = array.slice(Math.floor(array.length/2)+1);
+    return Node(value, buildTree(left), buildTree(right));
 }
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+        return;
+    }
+    if (node.right !== null) {
+        prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+        prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+};
+
+const test = buildTree([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]);
+prettyPrint(test);
+
