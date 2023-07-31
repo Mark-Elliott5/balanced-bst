69a19fc40ee19631ab6e0cf89dec2343b057a802
Mark Elliott <115184056+Mark-Elliott5@users.noreply.github.com>
2023-07-31 14:06:31 -0400

    Added delete function

--- balancedbst.js
+++ balancedbst.js
@@ -3,9 +3,9 @@ const Node = (data, left = null, right = null) => {
 }
 
 const Tree = (array) => {
-    debugger;
-    array.sort((a, b) => a - b);
-    const root = buildTree(array);
+    const sortedArray = array.sort((a, b) => a - b);
+    const filteredArray = sortedArray.filter((value, index) => sortedArray.indexOf(value) === index);
+    const root = buildTree(filteredArray);
     return root;
 }
 
@@ -23,15 +23,46 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
     if (node === null) {
         return;
     }
-    if (node.right !== null) {
+    if (node.right) {
         prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
     }
     console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
-    if (node.left !== null) {
+    if (node.left) {
         prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
     }
 };
 
-const test = buildTree([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]);
-prettyPrint(test);
+const deleteNode = (value, tree) => {
+    if (tree === null) {
+        return;
+    }
+    if (tree.data != value) {
+        if (tree.data < value) {
+            tree.right = deleteNode(value, tree.right);
+        } else {
+            tree.left = deleteNode(value, tree.left);
+        }
+    } else {
+        if (!tree.left) {
+            return tree.right;
+        } if (!tree.right) {
+            return tree.left;
+        }
+        tree.data = getMinValue(tree.right);
+        tree.right = deleteNode(tree.data, tree.right);
+    }
+    prettyPrint(tree);
+    return tree;
+}
 
+const getMinValue = (tree) => {
+    let minValue = tree.data;
+    while (tree.left) {
+        minValue = tree.left.data;
+        tree = tree.left;
+    }
+    return minValue;
+}
+
+const test = Tree([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]);
+prettyPrint(test);
